c6cf7733bf22934dac936d3657105b351f481c62
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-09-27 20:09:45 +0000

    idek

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ pwm = GPIO.PWM(p, 100) # create PWM object @ 100 Hz
 try:
   pwm.start(0) # initiate PWM at 0% duty cycle
   while 1:
-    for dc in range(100,0): # loop duty cycle from 0 to 100
+    for dc in range(100,0): # loop duty cycle from 0 to 100 b
       pwm.ChangeDutyCycle(dc) # set duty cycle
       sleep(0.01) # sleep 10 ms
 except KeyboardInterrupt: # stop gracefully on ctrl-C
