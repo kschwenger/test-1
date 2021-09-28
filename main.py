a597c282a7e3256e005343c6ee0bc1ad2caac7f3
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-09-28 01:46:26 +0000

    revert

--- main.py
+++ main.py
@@ -15,25 +15,17 @@ gpio.setup(led3, gpio.OUT)
 pwm = gpio.PWM(led1, 100) # create PWM object @ 100 Hz
 
 def button_callback(channel):
-  if pressed == True:
-    pressed = False
-  elif pressed == False:
-    pressed = True
-
-  while pressed == True:
-    print("Button was switched on pin %d" % channel)
-    pwm.start(0) # initiate PWM at 0% duty cycle
-    for dc in range(101): # loop duty cycle from 0 to 100
-      pwm.ChangeDutyCycle(dc) # set duty cycle
-      sleep(0.01) # sleep 10 ms
-    sleep(.2)
-    pwm.start(100)
-    for dc in range(100,-1,-1):
-      pwm.ChangeDutyCycle(dc)
-      sleep(0.01)
-    sleep(.2)
-
-pressed = True
+  print("Button was switched on pin %d" % channel)
+  pwm.start(0) # initiate PWM at 0% duty cycle
+  for dc in range(101): # loop duty cycle from 0 to 100
+    pwm.ChangeDutyCycle(dc) # set duty cycle
+    sleep(0.01) # sleep 10 ms
+  sleep(.2)
+  pwm.start(100)
+  for dc in range(100,-1,-1):
+    pwm.ChangeDutyCycle(dc)
+    sleep(0.01)
+  sleep(.2)
 
 gpio.add_event_detect(in1,gpio.BOTH,callback=button_callback) # Setup event on pin 20 rising edge
 gpio.add_event_detect(in2,gpio.BOTH,callback=button_callback) # Setup event on pin 21 rising edge
