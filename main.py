02d2034d3bede1b111c6abcb32208fd03b7294ef
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-09-28 22:46:56 +0000

    test 1 hz

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ led1, led2, led3 = 4, 27, 13
 gpio.setmode(gpio.BCM) 
 gpio.setup(in1, gpio.IN, pull_up_down=gpio.PUD_DOWN) # Set pin 20 to be an input pin and set initial value to be pulled low (off)
 gpio.setup(in2, gpio.IN, pull_up_down=gpio.PUD_DOWN) # Set pin 21 to be an input pin and set initial value to be pulled low (off)
-gpio.setup(led1, gpio.OUT)
+gpio.setup(led1, gpio.OUT)      # set led pins as output pins
 gpio.setup(led2, gpio.OUT)
 gpio.setup(led3, gpio.OUT)
 
-pwm1 = gpio.PWM(led1, 100) # create PWM object @ 100 Hz
-pwm2 = gpio.PWM(led2, 100) # create PWM NEED to add this
+pwm1 = gpio.PWM(led1, 1) # create PWM object @ 100 Hz for both leds
+pwm2 = gpio.PWM(led2, 1) 
 
 def button_callback(channel):
   if channel == in1:
