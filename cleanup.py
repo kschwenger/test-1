6cf0216e0721e1a68198296b30b3e92d7b0036ba
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-09-28 00:58:48 +0000

    added cleanup file

---
+++ cleanup.py
@@ -0,0 +1,2 @@
+import RPi.GPIO as gpio # Import Raspberry Pi GPIO library
+gpio.cleanup() # Clean up
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+"""
 import RPi.GPIO as gpio # Import Raspberry Pi GPIO library
 in1, in2 = 20, 21
 led1, led2, led3 = 4, 27, 13
 gpio.setmode(gpio.BCM) # Use physical pin numbering
+gpio.setup(in1, gpio.IN, pull_up_down=gpio.PUD_DOWN) # Set pin 20 to be an input pin and set initial value to be pulled low (off)
+gpio.setup(in2, gpio.IN, pull_up_down=gpio.PUD_DOWN) # Set pin 21 to be an input pin and set initial value to be pulled low (off)
 gpio.setup(led1, gpio.OUT)
 gpio.setup(led2, gpio.OUT)
 gpio.setup(led3, gpio.OUT)
@@ -10,11 +13,11 @@ def button_callback(pin, channel):
     print("Button was pushed on pin %d" % channel)
     gpio.output(pin, 1)
 gpio.setwarnings(False) # Ignore warning for now
-gpio.setup(in1, gpio.IN, pull_up_down=gpio.PUD_DOWN) # Set pin 20 to be an input pin and set initial value to be pulled low (off)
-gpio.setup(in2, gpio.IN, pull_up_down=gpio.PUD_DOWN) # Set pin 20 to be an input pin and set initial value to be pulled low (off)
+
 
 gpio.add_event_detect(in2,gpio.RISING,callback=button_callback) # Setup event on pin 20 rising edge
 
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 
 gpio.cleanup() # Clean up
+"""
